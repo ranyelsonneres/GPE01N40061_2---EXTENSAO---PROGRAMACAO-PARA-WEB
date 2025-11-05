34af9431dd1d60af178d46caeb01c7432b1c099c
Ranyelson Neres <ranyelson.carvalho@p.ucb.br>
2025-11-04 21:24:48 -0300

    Update server.js

--- Projeto-Crud/server.js
+++ Projeto-Crud/server.js
@@ -16,6 +16,15 @@ app.get('/', (req, res) =>{
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+//trazer as rotas presentes em API.js
+const apiRoutes = require('./routes/api');
+
+//receber do front-end (dados no formato JSON)
+app.use(express.json());
+
+//mandar para a rota que faz o cadastro
+app.use('/api/users/', apiRoutes);
+
 
 app.listen(port, ()=>{
     console.log("Servidor funcionando!!");
