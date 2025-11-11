41033595e65027fba80557f44dc1e6eb96fba52f
Ranyelson Neres <ranyelson.carvalho@p.ucb.br>
2025-11-11 16:23:35 -0300

    Update api.js

--- Projeto-Crud/routes/api.js
+++ Projeto-Crud/routes/api.js
@@ -20,5 +20,39 @@ router.post('/', (req, res)=>{
     );
 });
 
+//rota para listar os usuários do banco
+router.get('/', (req, res) => {
+    //executar a consulta SQL
+    db.query('SELECT * FROM users', (err, results) =>{
+        if(err) return res.status(500).send(err);
+        res.json(results);
+    });
+});
+
+//rota para editar, pode-se utilizar o Patch para atualizar partes específicas
+router.put('/:id', (req, res) => {
+    //extrair os dados
+    const {nome, email} = req.body;
+    const {id} = req.params;
+
+    //executar a instrução SQL
+    db.query('UPDATE users SET nome = ?, email = ? WHERE id = ?', [nome, email, id], (err) =>{
+        if(err) return res.status(500).send(err);
+        res.json({id, nome, email});
+    });
+});
+
+router.delete('/:id', (req, res) =>{
+    //pegar o ID do usuário
+    const {id} = req.params;
+
+    //executar a instrução SQL
+    db.query('DELETE FROM users WHERE id = ?', [id], (err)=>{
+        if(err) return res.status(500).send(err);
+        res.sendStatus(204);
+    });
+});
+
+
 //exportar o módulo de rotas: router
 module.exports = router;
